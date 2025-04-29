8bbbe798905bb1697f00629988b0e004c7e6db28
Marco Dennstädt <M.Dennstaedt@gsi.de>
2025-04-29 16:08:23 +0200

    Fix build with yocto-sdk version 4.1.1

--- cmake/FindEtherbone.cmake
+++
@@ -1,9 +0,0 @@
-
-set(Etherbone_VERSION ${PC_Etherbone_VERSION})
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Etherbone
-  REQUIRED_VARS
-    Etherbone_LIBRARY
-    Etherbone_INCLUDE_DIR
-  VERSION_VAR Etherbone_VERSION
-)
