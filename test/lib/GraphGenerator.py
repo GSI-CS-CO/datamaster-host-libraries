c3bd110aa83be1b351ec50b1249d7b6e6458fee4
Marco Dennstädt <M.Dennstaedt@gsi.de>
2025-06-13 12:47:28 +0000

    More WIP

--- test/lib/GraphGenerator.py
+++
@@ -1,7 +0,0 @@
-
-class RootGraph():
-    def __init__(self, name):
-        self.name = name
-    
-    def __str__(self) -> str:
-        return f"digraph {self.name} {{}}"
--- test/lib/TestContext.py
+++ test/lib/TestContext.py
@@ -1,5 +1,6 @@
 from behave import runner
 from test.features.device_config import DUT
+import pydot
 
 class TestContext(runner.Context):
     """
@@ -8,3 +9,18 @@ class TestContext(runner.Context):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.dut : (DUT|None) = None  # Device Under Test
+        self.graphs : dict[str, pydot.Dot] = {}  # Store graphs by name
+
+    def add_graph(self, name: str, graph: pydot.Dot):
+        """
+        Add a graph to the context.
+        :param name: The name of the graph.
+        :param graph: The pydot graph object.
+        """
+        if not isinstance(graph, pydot.Dot):
+            raise TypeError("graph must be an instance of pydot.Dot")
+        
+        if name in self.graphs:
+            raise ValueError(f"Graph with name '{name}' already exists in the context.")
+
+        self.graphs[name] = graph
